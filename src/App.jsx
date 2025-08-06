a676fa51a7e201a1829d9b807fe86ad3eb59ea32
Sujan Sah <sujansah.dev@gmail.com>
2025-08-06 12:13:43 +0545

    feature:made some protected pages public

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,39 @@
+import authService from "./appwrite/auth";
+import databaseService from "./appwrite/databaseService.js"
+import userDataService from "./appwrite/userDataService.js"
+import { login, logOut } from "./store/authSlice";
+import { mutatePost } from "./store/postSlice"
+import { mutateLikedPost } from "./store/userDataSlice.js";
+import { useState,useEffect } from "react";
+import { useDispatch,} from "react-redux";
+import Loader from "./Components/Loader"
+
 import { Outlet } from "react-router-dom"
 import Header from "./Components/Header/Header"
 import Footer from "./Components/Footer/Footer"
 import { ToastContainer} from "react-toastify"
 
 function App(){
+  const [isLoading,setIsLoading]=useState(true)
+  const dispatch=useDispatch()
+  useEffect(()=>{
+    databaseService
+    .getAllPost()
+    .then((posts) => dispatch(mutatePost(posts.documents)))
+    authService?.getCurrentUser().then((user)=>{
+     if(user){
+     userDataService.getUser(user.$id).then(userData=>dispatch(mutateLikedPost(userData?.likedPost || [])))
+    dispatch(login(user))
+     }
+     else{
+       dispatch(logOut())
+     }
+    })
+    .finally(()=>setIsLoading(false))
+ },[])
+ if(isLoading)return <div className="min-h-screen w-full flex justify-center items-center">
+   <Loader/>
+ </div>
   return <>
     <ToastContainer
          position="top-right"
--- src/Router.jsx
+++ src/Router.jsx
@@ -4,7 +4,9 @@ import {
   Route,
   Navigate,
   Outlet,
+  useNavigate,
 } from "react-router-dom";
+import { useSelector } from "react-redux";
 import App from "./App";
 import Home from "./Pages/Home";
 import ArticleDetails from "./Pages/ArticleDetails";
@@ -17,20 +19,19 @@ import NotFound from "./Pages/NotFound";
 import UserProfile from "./Pages/UserProfile"
 import AllPosts from "./Pages/AllPosts";
 import Search from "./Pages/Search"
-import ProtectedRoutes from "./ProtectedRoutes";
 export const router = createBrowserRouter(
   createRoutesFromElements(
     <>
-      <Route element={<ProtectedRoutes/>}>
         <Route path="/" element={<App />}>
           <Route path="" element={<Home />} />
-          <Route path="user/:currentUser" element={<UserProfile />} />
           <Route path="article" element={<AllPosts/>} />
           <Route path="search" element={<Search/>} />
-          <Route path="article/:id" element={<ArticleDetails />} />
+          <Route path="article/:id" element={<ArticleDetails/>} />
+          <Route path="about" element={<About />} />
+      <Route element={<ProtectedRoutes/>}>
+          <Route path="user/:currentUser" element={<UserProfile />} />
           <Route path="edit/article/:id" element={<EditArticle/>} />
           <Route path="create" element={<CreateArticle />} />
-          <Route path="about" element={<About />} />
         </Route>
       </Route>
       <Route path="/signup" element={<Signup />} />
@@ -40,4 +41,22 @@ export const router = createBrowserRouter(
   )
 );
 
-  
+
+ function ProtectedRoutes(){
+    const isAuthenticated=useSelector(state=>state.auth.status)
+    const navigate =useNavigate()
+    return isAuthenticated ? <Outlet/> :   <div className="min-h-screen flex items-center justify-center dark:bg-slate-900 px-4">
+    <div className="bg-white dark:bg-dark-primary shadow-[6px_6px_12px_#c5c5c5] dark:shadow-[6px_6px_12px_#000] rounded-xl p-8 max-w-md text-center">
+      <h2 className="text-2xl font-bold text-gray-800 mb-4">🔒 Access Denied</h2>
+      <p className="text-gray-600 mb-6">
+        You must be logged in to view this page.
+      </p>
+      <button
+        onClick={() => navigate("/login")}
+        className="bg-accent font-semibold text-white px-6 py-2 rounded-md hover:scale-105 active:scale-95  transition duration-200"
+      >
+        Go to Login
+      </button>
+    </div>
+  </div>
+  }
