95bf66b9dc1de7115be6032a6ef8212f4feb8ede
Sujan Sah <sujansah.dev@gmail.com>
2025-09-01 11:10:16 +0545

    fix:minor data leak

--- src/appwrite/auth.js
+++ src/appwrite/auth.js
@@ -34,19 +34,10 @@ export class AuthService {
      try {
          return await this.account.get()
      } catch (error) {
-        console.log("appwrite error: currentuser",error)
+        return null;
      }
-     return null;
   }
-  async loginWithOAuth2(){
- try{
-   this.account.createOAuth2Session('google', 'http://localhost:5173/', 'http://localhost:5173/login');
-   const session = await this.account.getSession('current');
-   return session
- }catch(error){
-     console.log(error.message || "appwrite error oauth login")
- }
-  }
+ 
   async logOut(){
     try{
          return await this.account.deleteSession("current") //use current to delete current session on logout to delete or logout from all devices use deletesessions() sessions not session
